88b7f3fd8e6dcac8c0235ccbba3a6c292beb59cc
dailoc1205 <pandablack1205@gmail.com>
2023-12-07 20:30:12 +0700

    [Add] Find All PostApi

--- src/main/java/com/vti/blogapp/controller/PostController.java
+++ src/main/java/com/vti/blogapp/controller/PostController.java
@@ -6,9 +6,14 @@ import com.vti.blogapp.service.PostService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @Getter
 @Setter
@@ -16,6 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class PostController {
     private final PostService postService;
+    @GetMapping("/api/v1/posts")
+    public Page<PostDto> findAll(Pageable pageable) {
+        return postService.findAll(pageable);
+    }
+
 
     @PostMapping("/api/v1/posts")
     public PostDto create(@RequestBody PostCreateFrom from){
